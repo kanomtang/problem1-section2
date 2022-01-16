46a4b5eae563ed46d24152dc26cc48f51158f1c2
Rungroj Sonsubanan <rungroj_s@cmu.ac.th>
2022-01-16 14:59:41 +0700

    feat: Create responsive app page following requirement - Have 3 columns 	- First column fixed width at 200px 	- Second column fixed min width at 100px 	- Third column fixed width at 300px 	- If screen size below 600px should display scroll on x-axis

--- problem1-section2/src/app/app.module.ts
+++ problem1-section2/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
